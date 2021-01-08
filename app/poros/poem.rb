93ba160ff91b9f5cf7e11e02b9e11ac6d425792d
Austin Aspaas <araspaas@live.com>
2021-01-08 10:04:35 -0700

    Add poem poro

---
+++ app/poros/poem.rb
@@ -0,0 +1,13 @@
+class Poem
+  attr_reader :title, :author, :full_text
+
+  def initialize(poem_params)
+    @title = poem_params[:title]
+    @author = poem_params[:author]
+    @full_text = poem_params[:lines].join("\n")
+  end
+
+  def tones
+    []
+  end
+end
